ddb2495168b28321413b602f2dd6160132d5fec2
roijjj <eddyyozombie@gmail.com>
2025-07-16 15:56:43 -0600

    Add table display and book management to library app
    Introduces a table-based UI for displaying books, including sorting, and adds buttons for removing books and marking them as read. Updates HTML to include an 'add' button and applies basic table styling in CSS.

--- library.js
+++ library.js
@@ -1,5 +1,6 @@
 const myLibrary = [];
 const container = document.getElementById("main")
+let currnetbook = 0;
 
 
 function Book(bid, name, auth,pages,read) {
@@ -20,25 +21,71 @@ myLibrary.push(books);
 
 
 function showbooks(){
-for(let x in myLibrary){
 
- createcard(myLibrary[x]);
+let tab = document.createElement("table");
+let tablehead = document.createElement("thead");
+let tablebody = document.createElement("tbody");
+let hrow= document.createElement("tr")
+let title = document.createElement("th");
+let author = document.createElement("th");
+let page = document.createElement("th");
+let read = document.createElement("th");
+let buttons = document.createElement("th");
+
+
+
+title.innerHTML = "Title";
+author.innerHTML = "author";
+page.innerHTML = "page number";
+read.innerHTML = "read";
+//buttons.innerHTML = "read";
+
+
+hrow.appendChild(title);
+hrow.appendChild(author);
+hrow.appendChild(page);
+hrow.appendChild(read);
+//hrow.appendChild(buttons)
+tablehead.appendChild(hrow);
+tab.appendChild(tablehead);
+
+let sortli = ()=> myLibrary.sort((myLibrary1, myLibrary2)=>{
+  let title1 = myLibrary1.title;
+  let title2 = myLibrary2.title;
+  if(title1<title2){
+    return 1; 
+  }
+  if(title1>title2){
+    return -1;
+  }
+  return 0;
+})
+
+for(let x in myLibrary){
+  
+
+ createcard(myLibrary[x], tablebody)
 
 }   
+tab.appendChild(tablebody)
+container.appendChild(tab)
 
 }
 
 
-function createcard(book){
-let card = document.createElement("div");
-let id = document.createElement("p");
-let title = document.createElement("p");
-let author = document.createElement("p");
-let page = document.createElement("p");
-let read = document.createElement("p");
+function createcard(book, tab){
+let card = document.createElement("tr");
+//let id = document.createElement("td");
+let title = document.createElement("td");
+let author = document.createElement("td");
+let page = document.createElement("td");
+let read = document.createElement("td");
+let rbutton = document.createElement("td");
+let removebook = document.createElement("button");
+let readbook = document.createElement("button");
 
 
-card.classList.add("bcard");
+//card.classList.add("bcard");
 title.classList.add("longname");
 author.classList.add("longname");
 page.classList.add("longname");
@@ -51,25 +98,35 @@ title.innerHTML = book.title;
 author.innerHTML = book.author;
 page.innerHTML = book.page;
 read.innerHTML = book.hasrad;
+removebook.innerHTML = "remove book";
+readbook.innerHTML = "read";
 
+removebook.addEventListener("click",function(){rbook(book)});
+readbook.addEventListener("click", function(){hasRead(book)});
 
-
+rbutton.appendChild(removebook);
+rbutton.appendChild(readbook);
 card.appendChild(title);
 card.appendChild(author);
 card.appendChild(page);
 card.appendChild(read);
-container.appendChild(card);
+card.appendChild(rbutton);
+tab.appendChild(card);
 }
-function removebook(){
+function rbook(book){
+myLibrary.splice(1, currnetbook)
+showbooks();
 
 }
-function hasRead(){
-
+function hasRead(book){
+book.hasrad = true;
+console.log(book.hasrad)
+showbooks();
 }
 
 console.log("b");
-addBookToLibrary("tom", "bob", "99","true");
-addBookToLibrary("td", "bo", "9","false");
+addBookToLibrary("tom", "bob", 99,false);
+addBookToLibrary("td", "bo", 9,false);
 console.log(myLibrary[0].id);
 showbooks();
 
